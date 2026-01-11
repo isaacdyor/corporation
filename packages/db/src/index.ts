dd0e9971195e83a4eb6b0fb18317d52443141990
Isaac Dyor <isaac@dyor.com>
2026-01-10 18:06:14 -0800

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,12 @@
+import { env } from "@corporation/env/server";
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import ws from "ws";
+
+import * as schema from "./schema";
+
+neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true;
+
+const sql = neon(env.DATABASE_URL || "");
+export const db = drizzle(sql, { schema });
